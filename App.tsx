202e74212f8f74f5731c8696153c037c742794c9
fred <mohammad.dahmardeh9868@gmail.com>
2023-05-08 23:54:15 +0330

    a bit of cleaning up

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Home } from "./src/screens/Home";
 
